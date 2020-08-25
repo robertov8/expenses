2f7791100f06bf0b829332f037bf7f9877defbb4
Roberto Ribeiro <robertov82008@gmail.com>
2020-08-24 22:21:58 -0300

    79 - Mapear Dados para Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -45,9 +45,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('Lista de Transações'),
-            elevation: 5,
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
           )
         ],
       ),
