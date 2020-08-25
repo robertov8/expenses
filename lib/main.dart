7ac96f91c46b9bd0d8a667abf631100fb13d7918
Roberto Ribeiro <robertov82008@gmail.com>
2020-08-24 21:17:25 -0300

    76 - Combinando Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,21 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Center(
-        child: Text('Versão inicial'),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+            elevation: 5,
+          )
+        ],
       ),
     );
   }
