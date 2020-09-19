6db47eae2f92762879907e35fae3f65ec71bff33
Roberto Ribeiro <robertov82008@gmail.com>
2020-09-19 18:43:16 -0300

    44. Finalizando Chart Widget

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,6 +1,7 @@
 import 'package:expenses/components/chart_bar.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
@@ -44,15 +45,22 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactions.map((transaction) {
-          return ChartBar(
-            label: transaction['day'],
-            value: transaction['value'],
-            percentage: (transaction['value'] as double) / _weekTotalValue,
-          );
-          return Text('${transaction['day']}: ${transaction['value']} ');
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((transaction) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: transaction['day'],
+                value: transaction['value'],
+                percentage: (transaction['value'] as double) / _weekTotalValue,
+              ),
+            );
+            return Text('${transaction['day']}: ${transaction['value']} ');
+          }).toList(),
+        ),
       ),
     );
   }
