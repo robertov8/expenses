196ce495fa7c3ffb3ed16a4d4d79973a313cb66b
Roberto Ribeiro <robertov82008@gmail.com>
2020-09-19 19:08:37 -0300

    46. Usando ListTile

--- lib/components/transaction_card.dart
+++ lib/components/transaction_card.dart
@@ -10,45 +10,28 @@ class TransactionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: 15,
-              vertical: 10,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Theme.of(context).primaryColor,
-                width: 2,
-              ),
-            ),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              'R\$ ${transaction.value.toStringAsFixed(2)}',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Theme.of(context).primaryColor,
-              ),
+      elevation: 5,
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: FittedBox(
+              child: Text('R\$${transaction.value.toStringAsFixed(2)}'),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                transaction.title,
-                style: Theme.of(context).textTheme.title,
-              ),
-              Text(
-                DateFormat('d MMM y').format(transaction.date),
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-              )
-            ],
-          )
-        ],
+        ),
+        title: Text(
+          transaction.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        subtitle: Text(
+          DateFormat('d MMM y').format(transaction.date),
+          style: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
       ),
     );
   }
